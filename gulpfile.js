4b700784777fad57a37e76dc970edc8deb1333d4
Matheus Fernandes <github@matheus.top>
2016-09-02 18:55:01 -0300

    Add Babel and uglify to `gulp build`

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
+const babel = require('gulp-babel');
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+const uglify = require('gulp-uglify');
 
 gulp.task('build', () => {
 	gulp.src('app/src/pug/*')
 		.pipe(pug())
+		.pipe(gulp.dest('app/dist'));
+
+	gulp.src('app/src/js/*')
+		.pipe(babel({presets: ['es2015']}))
+		.pipe(uglify())
 		.pipe(gulp.dest('app/dist'));
 });
