0fcafe4852e64d854d0a38afb86590cf1f9cedb5
Matheus Fernandes <github@matheus.top>
2016-09-02 17:20:59 -0300

    Move the app to the menubar :sparkles:

--- app/index.js
+++ app/index.js
@@ -1,31 +1,3 @@
-const {app, BrowserWindow} = require('electron');
-
-let win;
-
-function createWindow() {
-	win = new BrowserWindow({width: 800, height: 600});
-
-	win.loadURL(`file://${__dirname}/src/index.html`);
-
-	win.webContents.openDevTools();
-
-	win.on('closed', () => {
-		win = null;
-	});
-}
-
-app.on('ready', () => {
-	createWindow();
-});
-
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
-
-app.on('activate', () => {
-	if (win === null) {
-		createWindow();
-	}
+const menubar = require('menubar')({
+	index: `file://${__dirname}/src/index.html`
 });
