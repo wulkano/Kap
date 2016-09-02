885d423e6562159ce009e35635495b6c6989fe3b
Matheus Fernandes <github@matheus.top>
2016-09-02 18:52:48 -0300

    Open dev tools alongside with the window

--- app/index.js
+++ app/index.js
@@ -1,3 +1,7 @@
-require('menubar')({
+const menubar = require('menubar')({
 	index: `file://${__dirname}/dist/index.html`
+});
+
+menubar.on('after-create-window', () => {
+	menubar.window.openDevTools();
 });
