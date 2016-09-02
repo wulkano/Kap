6eb66c78bd91447a90797cda1ef4283a23ea0c5e
Matheus Fernandes <github@matheus.top>
2016-09-02 18:56:13 -0300

    Fix: run the code after the DOM is ready

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,36 +1,38 @@
-const aperture = require('aperture.js')();
+document.addEventListener('DOMContentLoaded', () => {
+	const aperture = require('aperture.js')();
 
-function startRecording() {
-	const past = Date.now();
+	function startRecording() {
+		const past = Date.now();
 
-	aperture.startRecording()
-		.then(() => {
-			console.log(`Started recording after ${(Date.now() - past) / 1000}s`);
-		})
-		.catch(console.error);
-}
-
-function askUserToSaveFile(opts) {
-	if (!opts.filePath || !opts.fileName) {
-		throw new Error('askUserToSaveFile must be called with {filePath, fileName}');
+		aperture.startRecording()
+			.then(() => {
+				console.log(`Started recording after ${(Date.now() - past) / 1000}s`);
+			})
+			.catch(console.error);
 	}
 
-	const a = document.createElement('a');
-	a.style.display = 'none';
-	a.href = opts.filePath;
-	a.download = opts.fileName;
-	document.body.appendChild(a);
-	a.click();
-}
+	function askUserToSaveFile(opts) {
+		if (!opts.filePath || !opts.fileName) {
+			throw new Error('askUserToSaveFile must be called with {filePath, fileName}');
+		}
 
-function stopRecording() {
-	aperture.stopRecording()
-		.then(filePath => {
-			const fileName = `Screen record ${Date()}.mp4`;
-			askUserToSaveFile({fileName, filePath});
-		});
-}
+		const a = document.createElement('a');
+		a.style.display = 'none';
+		a.href = opts.filePath;
+		a.download = opts.fileName;
+		document.body.appendChild(a);
+		a.click();
+	}
 
-document.querySelector('#start').onclick = startRecording;
+	function stopRecording() {
+		aperture.stopRecording()
+			.then(filePath => {
+				const fileName = `Screen record ${Date()}.mp4`;
+				askUserToSaveFile({fileName, filePath});
+			});
+	}
 
-document.querySelector('#stop').onclick = stopRecording;
+	document.querySelector('#start').onclick = startRecording;
+
+	document.querySelector('#stop').onclick = stopRecording;
+});
