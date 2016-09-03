ab612a1e43f7d66ecef3dd08745868c827c158cf
Matheus Fernandes <github@matheus.top>
2016-09-02 23:01:52 -0300

    Add css processing to `gulp build`

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const babel = require('gulp-babel');
+const cssnano = require('cssnano');
 const gulp = require('gulp');
+const postcss = require('gulp-postcss');
 const pug = require('gulp-pug');
 const uglify = require('gulp-uglify');
 
 gulp.task('build', () => {
 	gulp.src('app/src/pug/*')
 		.pipe(pug())
+		.pipe(gulp.dest('app/dist'));
+
+	gulp.src('app/src/css/*')
+		.pipe(postcss([cssnano]))
 		.pipe(gulp.dest('app/dist'));
 
 	gulp.src('app/src/js/*')
