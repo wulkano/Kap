e0fb2cee3f9451c3803e4708a7de2219269205ff
Matheus Fernandes <github@matheus.top>
2016-09-24 19:23:46 -0300

    Reorganize the gulp build/watch flow

--- gulpfile.js
+++ gulpfile.js
@@ -9,27 +9,36 @@ const postcsssSimpleVars = require('postcss-simple-vars');
 const pug = require('gulp-pug');
 const uglify = require('gulp-uglify');
 
-gulp.task('build', () => {
-  gulp.src('app/src/pug/*')
-    .pipe(pug())
-    .pipe(injectSvg())
-    .pipe(gulp.dest('app/dist'));
-
-  gulp.src('app/src/css/*')
-    .pipe(postcss([postcsssSimpleVars, postcssExtend, postcssNested, cssnano]))
-    .pipe(gulp.dest('app/dist'));
-
-  gulp.src('app/src/js/*')
-    .pipe(babel({presets: ['es2015']}))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/dist'));
-
+gulp.task('build:main', () =>
   gulp.src('app/main.js')
     .pipe(babel({presets: ['es2015']}))
     .pipe(uglify())
-    .pipe(gulp.dest('app/dist'));
-});
+    .pipe(gulp.dest('app/dist')));
+
+gulp.task('build:pug', () =>
+  gulp.src('app/src/pug/*')
+    .pipe(pug())
+    .pipe(injectSvg())
+    .pipe(gulp.dest('app/dist')));
+
+gulp.task('build:css', () =>
+  gulp.src('app/src/css/*')
+    .pipe(postcss([postcsssSimpleVars, postcssExtend, postcssNested, cssnano]))
+    .pipe(gulp.dest('app/dist')));
+
+gulp.task('build:js', () =>
+  gulp.src('app/src/js/*')
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/dist')));
+
+gulp.task('build', ['build:main', 'build:pug', 'build:css', 'build:js']);
+
+gulp.task('default', ['build']);
 
 gulp.task('watch', ['build'], () => {
-  gulp.watch('app/src/**', ['build']);
+  gulp.watch('app/src/pug/*.pug', ['build:pug']);
+  gulp.watch('app/src/css/*.css', ['build:css']);
+  gulp.watch('app/src/js/*.js', ['build:js']);
+  gulp.watch('app/main.js', ['build:main']);
 });
