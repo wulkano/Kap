885421ec3825b99064c8b7e66fad1d60a86ab55d
Matheus Fernandes <github@matheus.top>
2016-09-27 16:12:01 -0300

    Do not send errors to Sentry in development

--- app/src/js/error-report.js
+++ app/src/js/error-report.js
@@ -1,4 +1,8 @@
-const raven = require('raven');
+const isDev = require('electron-is-dev');
 
-const ravenClient = new raven.Client('https://dde0663d852241628dca445a0b28d3f1:354142c4b46c4894b3ba876ce803bb6f@sentry.io/101586');
-ravenClient.patchGlobal();
+if (!isDev) {
+  const raven = require('raven');
+
+  const ravenClient = new raven.Client('https://dde0663d852241628dca445a0b28d3f1:354142c4b46c4894b3ba876ce803bb6f@sentry.io/101586');
+  ravenClient.patchGlobal();
+}
