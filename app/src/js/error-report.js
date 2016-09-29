453e502954366303800d9f8448688803c2c5b8b9
Matheus Fernandes <github@matheus.top>
2016-09-29 14:34:05 -0300

    Expose method to manually report an Error

--- app/src/js/error-report.js
+++ app/src/js/error-report.js
@@ -1,8 +1,17 @@
 const isDev = require('electron-is-dev');
 
+let ravenClient;
 if (!isDev) {
   const raven = require('raven');
 
-  const ravenClient = new raven.Client('https://dde0663d852241628dca445a0b28d3f1:354142c4b46c4894b3ba876ce803bb6f@sentry.io/101586');
+  ravenClient = new raven.Client('https://dde0663d852241628dca445a0b28d3f1:354142c4b46c4894b3ba876ce803bb6f@sentry.io/101586');
   ravenClient.patchGlobal();
 }
+
+function report(err) {
+  if (!isDev && err) {
+    ravenClient.captureException(err);
+  }
+}
+
+exports.report = report;
