6384a37901dcdf4cf626f44b21d9fb473e926052
Matheus Fernandes <github@matheus.top>
2016-10-11 22:25:59 -0300

    Move `reporter.js` to `app/src/common` Since it's used on both main and rederer processes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@ import postcssNested from 'postcss-nested';
 import postcsssSimpleVars from 'postcss-simple-vars';
 import pug from 'gulp-pug';
 
-gulp.task('build:main', () =>
+gulp.task('build:js:main', () =>
   gulp.src('app/src/main/*.js')
     .pipe(babel())
     .pipe(gulp.dest('app/dist')));
@@ -24,18 +24,24 @@ gulp.task('build:css', () =>
     .pipe(postcss([postcsssSimpleVars, postcssExtend, postcssNested, cssnano]))
     .pipe(gulp.dest('app/dist')));
 
-gulp.task('build:js', () =>
+gulp.task('build:js:renderer', () =>
   gulp.src('app/src/renderer/js/*.js')
     .pipe(babel())
     .pipe(gulp.dest('app/dist')));
 
-gulp.task('build', ['build:main', 'build:pug', 'build:css', 'build:js']);
+gulp.task('build:js:common', () =>
+  gulp.src('app/src/common/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('app/dist')));
+
+gulp.task('build', ['build:js:main', 'build:pug', 'build:css', 'build:js:renderer', 'build:js:common']);
 
 gulp.task('default', ['build']);
 
 gulp.task('watch', ['build'], () => {
   gulp.watch('app/src/renderer/pug/*.pug', ['build:pug']);
   gulp.watch('app/src/renderer/css/*.css', ['build:css']);
-  gulp.watch('app/src/renderer/js/*.js', ['build:js']);
-  gulp.watch('app/src/main/*.js', ['build:main']);
+  gulp.watch('app/src/renderer/js/*.js', ['build:js:renderer']);
+  gulp.watch('app/src/common/*.js', ['build:js:common']);
+  gulp.watch('app/src/main/*.js', ['build:js:main']);
 });
