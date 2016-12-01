af19a4458d11b438c75c00f2bce620c08686e70d
Matheus Fernandes <github@matheus.top>
2016-11-30 23:40:43 -0200

    Switch between prefs sections

--- app/src/renderer/js/preferences.js
+++ app/src/renderer/js/preferences.js
@@ -9,8 +9,12 @@ const settingsValues = app.kap.settings.getAll();
 
 document.addEventListener('DOMContentLoaded', () => {
   // Element definitions
+  const advancedPrefs = $('.advanced-prefs');
+  const advancedPrefsBtn = $('.show-advanced-prefs');
   const allowAnalyticsCheckbox = $('#allow-analytics');
   const chooseSaveDirectoryBtn = $('.js-choose-save');
+  const generalPrefs = $('.general-prefs');
+  const generalPrefsBtn = $('.show-general-prefs');
   const header = $('header');
   const openOnStartupCheckbox = $('#open-on-startup');
   const saveToDescription = $('.js-save-to-description');
@@ -25,6 +29,22 @@ document.addEventListener('DOMContentLoaded', () => {
   saveToDescription.innerText = `.../${settingsValues.kapturesDir.split('/').pop()}`;
   openOnStartupCheckbox.checked = settingsValues.openOnStartup;
   allowAnalyticsCheckbox.checked = settingsValues.allowAnalytics;
+
+  generalPrefsBtn.onclick = function (e) {
+    e.preventDefault();
+    this.classList.add('is-active');
+    advancedPrefsBtn.classList.remove('is-active');
+    generalPrefs.classList.remove('hidden');
+    advancedPrefs.classList.add('hidden');
+  };
+
+  advancedPrefsBtn.onclick = function (e) {
+    e.preventDefault();
+    this.classList.add('is-active');
+    generalPrefsBtn.classList.remove('is-active');
+    advancedPrefs.classList.remove('hidden');
+    generalPrefs.classList.add('hidden');
+  };
 
   chooseSaveDirectoryBtn.onclick = function () {
     const directories = dialog.showOpenDialog(electronWindow, {properties: ['openDirectory']});
