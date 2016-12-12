36c52dd65f679f940c1c9d0ce74b4483f5e657de
Matheus Fernandes <github@matheus.top>
2016-12-12 03:44:53 -0200

    Fetch a input device when the app boots

--- app/src/common/settings-manager.js
+++ app/src/common/settings-manager.js
@@ -4,13 +4,16 @@ import {app} from 'electron';
 import settings from 'electron-settings';
 import objectPath from 'object-path';
 
+const aperture = require('aperture.js')();
+
 const DEFAULTS = {
   kapturesDir: `${homedir()}/Movies/Kaptures`,
   openOnStartup: false,
   allowAnalytics: true,
   showCursor: true,
   highlightClicks: false,
-  fps: 30
+  fps: 30,
+  recordAudio: false
 };
 
 const volatiles = {
@@ -34,9 +37,18 @@ function sync() {
 }
 
 function init() {
-  settings.defaults(DEFAULTS);
-  settings.applyDefaultsSync();
-  sync();
+  // we need to fetch a input device because if the user opens the app for the first time
+  // and toggle the mic in the main window to record audio, we will not record any audio
+  // if we do not have a input id stored.
+  // TODO: if no input device is available (could happen in an iMac, for example), we need
+  // to tell the user
+  // TODO: rewrite this comment (it's 4AM and i'm fucking tired)
+  aperture.getAudioSources().then(devices => {
+    DEFAULTS.audioInputDeviceId = (devices && devices[0] && devices[0].id) || 'none';
+    settings.defaults(DEFAULTS);
+    settings.applyDefaultsSync();
+    sync();
+  });
 }
 
 function get(key) {
