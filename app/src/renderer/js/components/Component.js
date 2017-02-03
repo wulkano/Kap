65f7e16baa94fdf78a02e43f7323ec56a2b168b7
Matheus Fernandes <github@matheus.top>
2017-02-03 02:36:55 -0200

    Add a base Component

---
+++ app/src/renderer/js/components/Component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import css from '../../css/styles'
+
+export default class Component extends React.Component {
+  constructor() {
+    super()
+    this.css = css
+  }
+}
