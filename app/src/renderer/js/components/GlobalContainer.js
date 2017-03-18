f3938d340917ff44263349893cde51597ae24d76
Matheus Fernandes <github@matheus.top>
2017-03-18 19:16:45 -0300

    Introduce `GlobalContainer`

---
+++ app/src/renderer/js/components/GlobalContainer.js
@@ -0,0 +1,54 @@
+import React from 'React'
+
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+import eightpoint from 'raw-loader!eightpoint/dist/eight.min.css'
+
+import Component from './Component'
+
+let css
+
+export default Child => {
+  const GlobalContainer = class extends Component {
+
+    constructor() {
+      super()
+      css = this.css
+    }
+    render() {
+      return (
+        <div>
+          <Child/>
+          <style jsx global>{`
+            ${eightpoint}
+            body {
+              color: ${css.colorSecondary};
+              font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue";
+              overflow: hidden;
+              letter-spacing: -.010rem;
+              border-radius: 5px;
+              overflow: hidden;
+              font-size: 1.4rem;
+            }
+
+            .webkit-drag {
+              -webkit-app-region: drag;
+              cursor: -webkit-grab;
+            }
+
+            .hidden {
+              opacity: 0 !important;
+              pointer-events: none !important;
+            }
+
+            .no-select {
+              -webkit-user-select: none !important;
+              cursor: default !important;
+            }
+          `}</style>
+        </div>
+      )
+    }
+  }
+
+  return GlobalContainer
+}
--- app/src/renderer/js/components/MainWindow.js
+++ app/src/renderer/js/components/MainWindow.js
@@ -1,25 +1,17 @@
 import React from 'react'
 
-// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
-import eightpoint from 'raw-loader!eightpoint/dist/eight.min.css'
-
 import Component from './Component'
 import Kap from './Kap'
 import TrayArrow from './TrayArrow'
 import WindowHeader from './WindowHeader'
+import GlobalContainer from './GlobalContainer'
 
 // webpack stuff
 /* eslint-disable import/no-unassigned-import */
 require('../../css/main.css')
 /* eslint-enable import/no-unassigned-import */
 
-let css
-
 class MainWindow extends Component {
-  constructor() {
-    super()
-    css = this.css
-  }
 
   render() {
     const {stuck: isStuck} = this.props.windows.main
@@ -33,38 +25,9 @@ class MainWindow extends Component {
           minimize={minimize}
           title="Kap"
           />
-        <main>
-        </main>
-        <style jsx global>{`
-          ${eightpoint}
-          body {
-            color: ${css.colorSecondary};
-            font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue";
-            overflow: hidden;
-            letter-spacing: -.010rem;
-            border-radius: 5px;
-            overflow: hidden;
-            font-size: 1.4rem;
-          }
-
-          .webkit-drag {
-            -webkit-app-region: drag;
-            cursor: -webkit-grab;
-          }
-
-          .hidden {
-            opacity: 0 !important;
-            pointer-events: none !important;
-          }
-
-          .no-select {
-            -webkit-user-select: none !important;
-            cursor: default !important;
-          }
-        `}</style>
       </div>
     )
   }
 }
 
-export default Kap(MainWindow, 'mainWindow')
+export default GlobalContainer(Kap(MainWindow, 'mainWindow'))
