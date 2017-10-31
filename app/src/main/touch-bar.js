7c514a919090a1febfad1b7e6c1216f6246daf49
Sindre Sorhus <sindresorhus@gmail.com>
2017-10-31 20:07:22 +0700

    Add TODO comment about using native icons

--- app/src/main/touch-bar.js
+++ app/src/main/touch-bar.js
@@ -90,6 +90,14 @@ const createFormatPopover = ({format, services, onSelectPlugin}) => {
 export const createEditorTouchbar = ({isPlaying, onDiscard, onSelectPlugin, onTogglePlay}) => {
   const shareServices = plugins.getShareServicesPerFormat();
   const controlButton = new TouchBarButton({
+    // TODO: When we're on Electron 1.8, we can use `nativeImage.createFromNamedImage`
+    // to get the native icon for play and pause.
+    // https://developer.apple.com/documentation/appkit/nsimagenametouchbarplaytemplate
+    // https://developer.apple.com/documentation/appkit/nsimagenametouchbarpausetemplate
+    // And for record:
+    // https://developer.apple.com/documentation/appkit/nsimagenametouchbarrecordstarttemplate
+    // We could also consider using the remove template for `Discard` depending on how it looks:
+    // https://developer.apple.com/documentation/appkit/nsimagenametouchbarremovetemplate
     label: isPlaying ? 'Pause' : 'Play',
     click: () => onTogglePlay(!isPlaying)
   });
