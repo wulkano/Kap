879c6aa1bc3af586e3955f7129d8d89220daef4b
Sindre Sorhus <sindresorhus@gmail.com>
2018-01-03 23:23:01 +0100

    Fix the implementation

--- app/src/common/reporter.js
+++ app/src/common/reporter.js
@@ -6,9 +6,16 @@ let Raven;
 
 function init() {
   if (!isDev) {
-    Raven = process.type === 'renderer' ? require('raven-js') : require('raven');
+    let dsn;
+    if (process.type === 'renderer') {
+      Raven = require('raven-js');
+      dsn = 'https://2dffdbd619f34418817f4db3309299ce@sentry.io/255536';
+    } else {
+      Raven = require('raven');
+      dsn = 'https://2dffdbd619f34418817f4db3309299ce:5d2b3b543a3a433abe928e39aff485fd@sentry.io/255536';
+    }
 
-    Raven.config('https://2dffdbd619f34418817f4db3309299ce@sentry.io/255536', {
+    Raven.config(dsn, {
       captureUnhandledRejections: false,
       tags: {
         process: process.type,
