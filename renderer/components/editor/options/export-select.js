1c40ad686601ba81d1dd040a15f3e7a185414162
Albin Ekblom <albinekblom@gmail.com>
2018-05-16 10:20:50 +0200

    Add export buttons

---
+++ renderer/components/editor/options/export-select.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Chevron = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="5" viewBox="0 0 10 5" className="c-select__toggle-icon" style={{marginLeft: '8px'}}>
+    <g fill="none" fillRule="evenodd" transform="translate(-7 -10)">
+      <polygon fill="#8C8C8C" points="7 10 12 15 17 10"/>
+      <polygon points="0 0 24 0 24 24 0 24"/>
+    </g>
+  </svg>
+);
+
+const Select = ({options}) => (
+  <select>
+    {options.map(option => <option key={option.value}>{option.label}</option>)}
+    <style jsx>{`
+    select {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      opacity: 0;
+    }
+  `}</style>
+  </select>
+);
+
+const ExportSelect = ({options = [], label}) => {
+  const hasOptions = options.length !== 0;
+  return (
+    <div className="root">
+      {hasOptions && <Select options={options}/>}
+      <button type="button"><span className="label">{label}</span>{hasOptions && <Chevron/>}</button>
+      <style jsx>{`
+        .root {
+          display: inline-block;
+          position: relative;
+          margin-right: 8px;
+        }
+        button {
+          appearance: none;
+          background-color: rgba(255,255,255,.10);
+          border: none;
+          border-radius: 4px;
+          color: #FFF;
+          font-size: 12px;
+          height: 24px;
+          padding: 0 8px;
+          transition: border .12s ease-in-out,background .12s ease-in-out;
+          display: flex;
+          align-items: center;
+          flex-direction: row;
+        }
+        button:focus {
+          outline: none;
+        }
+        button:focus, button:hover {
+          background-color: hsla(0,0%,100%,.2);
+        }
+      `}</style>
+    </div>
+  );
+};
+
+ExportSelect.propTypes = {
+  options: PropTypes.array,
+  label: PropTypes.string.isRequired
+};
+
+export default ExportSelect;
+
--- renderer/components/editor/options/index.js
+++ renderer/components/editor/options/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Size from './size';
 import Fps from './fps';
+import ExportSelect from './export-select';
+
+const TEST_OPTIONS = [{label: 'Export to disk', value: 'test'}]
 
 const Options = () => (
   <div className="options">
@@ -9,12 +12,16 @@ const Options = () => (
       <Size/>
     </div>
     <div>
-      <span className="label">FPS</span>
+      <span className="label fps">FPS</span>
       <Fps/>
     </div>
-    <div className="spacer" />
+    <div className="spacer"/>
     <div>
       <span className="label">Export</span>
+      <ExportSelect label="GIF" options={TEST_OPTIONS}/>
+      <ExportSelect label="MP4"/>
+      <ExportSelect label="WebM"/>
+      <ExportSelect label="APNG"/>
     </div>
     <style jsx>{`
       .spacer {
@@ -31,10 +38,16 @@ const Options = () => (
         display: flex;
         flex-direction: row;
         align-items: center;
+        padding: 0 16px;
+        padding-right: 8px;
       }
       .label {
         padding: 16px;
+        padding-left: 0;
         padding-right: 8px;
+      }
+      .label.fps {
+        margin-left: 16px;
       }
     `}</style>
   </div>
