1110ccc98dff11923312a8ee574a88e2b38eee7c
Albin Ekblom <albinekblom@gmail.com>
2018-09-22 16:41:37 +0200

    Add x svg

--- renderer/components/preferences/shortcut-input.js
+++ renderer/components/preferences/shortcut-input.js
@@ -108,7 +108,9 @@ export default class ShortcutInput extends React.Component {
           {this.renderKeys()}
           <input ref={this.inputRef} onKeyUp={this.store} onKeyDown={this.handleKeyDown} onBlur={this.handleBlur} onChange={noop}/>
         </div>
-        <button type="button" onClick={this.clearShortcut}>x</button>
+        <button type="button" onClick={this.clearShortcut}><svg style={{width: '24px', height: '24px'}} viewBox="0 0 24 24">
+          <path fill="#808080" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"/>
+        </svg></button>
         <style jsx>{`
           .shortcut-input {
             display: flex;
