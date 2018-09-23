cedc1deb7ce15f77e4871153338f01c327a8af93
Sindre Sorhus <sindresorhus@gmail.com>
2018-09-24 00:29:07 +0700

    Respect the `allowAnalytics` setting

--- main/utils/sentry.js
+++ main/utils/sentry.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const Sentry = require('@sentry/electron');
+const settings = require('../common/settings');
 
-Sentry.init({
-  dsn: 'https://2dffdbd619f34418817f4db3309299ce@sentry.io/255536'
-});
+if (settings.get('allowAnalytics')) {
+  Sentry.init({
+    dsn: 'https://2dffdbd619f34418817f4db3309299ce@sentry.io/255536'
+  });
+}
 
 module.exports = Sentry;
--- renderer/utils/sentry.js
+++ renderer/utils/sentry.js
@@ -1,7 +1,14 @@
+import electron from 'electron';
 import * as Sentry from '@sentry/electron';
 
-Sentry.init({
-  dsn: 'https://2dffdbd619f34418817f4db3309299ce@sentry.io/255536'
-});
+if (electron.remote) {
+  const settings = electron.remote.require('./common/settings');
+
+  if (settings.get('allowAnalytics')) {
+    Sentry.init({
+      dsn: 'https://2dffdbd619f34418817f4db3309299ce@sentry.io/255536'
+    });
+  }
+}
 
 export default Sentry;
