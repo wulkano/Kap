9b01a99caf300757f5ed850b45e80bd300aa6ba5
Sindre Sorhus <sindresorhus@gmail.com>
2018-09-24 00:54:59 +0700

    Only report to Sentry in production

--- main/utils/sentry.js
+++ main/utils/sentry.js
@@ -1,9 +1,10 @@
 'use strict';
 
+const {is} = require('electron-util');
 const Sentry = require('@sentry/electron');
 const settings = require('../common/settings');
 
-if (settings.get('allowAnalytics')) {
+if (!is.development && settings.get('allowAnalytics')) {
   Sentry.init({
     dsn: 'https://2dffdbd619f34418817f4db3309299ce@sentry.io/255536'
   });
