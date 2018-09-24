f28402f5dc7b68ee46bf62a223c7731c0276cb49
Sindre Sorhus <sindresorhus@gmail.com>
2018-09-24 20:17:33 +0700

    Update menus.js

--- main/menus.js
+++ main/menus.js
@@ -6,7 +6,7 @@ const {openPrefsWindow} = require('./preferences');
 
 const issueBody = `
 <!--
-Thank you for helping us test Kap, your feedback helps us make Kap better for everyone!
+Thank you for helping us test Kap. Your feedback helps us make Kap better for everyone!
 
 Before you continue; please make sure you've searched our existing issues to avoid duplicates. When you're ready to open a new issue, include as much information as possible. You can use the handy template below for bug reports.
 
