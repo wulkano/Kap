be05764f9cc979bc3192e8e2632c471ed954c2f8
George Karagkiaouris <gkaragkiaouris@quinnipiac.edu>
2018-09-24 09:22:35 -0400

    Add "Send Feedback…" menu item (#509)

--- main/menus.js
+++ main/menus.js
@@ -1,8 +1,34 @@
 'use strict';
 
-const {Menu} = require('electron');
-
+const os = require('os');
+const {Menu, shell, app} = require('electron');
 const {openPrefsWindow} = require('./preferences');
+
+const issueBody = `
+<!--
+Thank you for helping us test Kap. Your feedback helps us make Kap better for everyone!
+
+Before you continue; please make sure you've searched our existing issues to avoid duplicates. When you're ready to open a new issue, include as much information as possible. You can use the handy template below for bug reports.
+
+Step to reproduce:    If applicable, provide steps to reproduce the issue you're having.
+Current behavior:     A description of how Kap is currently behaving.
+Expected behavior:    How you expected Kap to behave.
+Workaround:           A workaround for the issue if you've found on. (this will help others experiencing the same issue!)
+-->
+
+**macOS version:**    ${process.platform} ${process.arch} ${os.release()}
+**Kap version:**      ${app.getVersion()}
+
+#### Steps to reproduce
+
+#### Current behavior
+
+#### Expected behavior
+
+#### Workaround
+
+<!-- If you have additional information, enter it below. -->
+`;
 
 const cogMenu = [
   {
@@ -15,6 +41,10 @@ const cogMenu = [
     label: 'Preferences…',
     accelerator: 'Cmd+,',
     click: openPrefsWindow
+  },
+  {
+    label: 'Send Feedback…',
+    click: () => shell.openExternal(`https://github.com/wulkano/kap/issues/new?body=${encodeURIComponent(issueBody)}`)
   },
   {
     type: 'separator'
