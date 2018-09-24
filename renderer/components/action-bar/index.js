f0b5a301f410ab929ee9195d046e46fa2a9b5699
George Karagkiaouris <karaggeorge0@gmail.com>
2018-09-24 11:24:55 -0400

    Fix shadow on the action bar

--- renderer/components/action-bar/index.js
+++ renderer/components/action-bar/index.js
@@ -57,7 +57,7 @@ class ActionBar extends React.Component {
               width: ${width}px;
               background: white;
               border-radius: 4px;
-              box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.2);
+              box-shadow: 0 20px 40px 0 rgba(0, 0, 0, .2);
               z-index: 10;
               top: ${y}px;
               left: ${x}px;
