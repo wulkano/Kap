72da7efa07d509f65de3b1e6bfd23bdaac4e3069
George Karagkiaouris <karaggeorge0@gmail.com>
2018-09-24 08:34:11 -0400

    Use body param instead of template

--- main/menus.js
+++ main/menus.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const {Menu, shell} = require('electron');
-
+const {Menu, shell, app} = require('electron');
+const os = require('os');
 const {openPrefsWindow} = require('./preferences');
 
 // Const {checkForUpdates} = require('./auto-updater');
@@ -20,6 +20,32 @@ const checkForUpdatesItem = {
   }
 };
 
+const issueBody = `
+<!--
+Thank you for helping us test the Kap Beta, your feedback helps us make Kap better for everyone!
+
+Before you continue; please make sure you've searched our existing issues to avoid duplicates. When you're ready to open a new issue, include as much information as possible. You can use the handy template below for bug reports.
+
+Step to reproduce:    If applicable, provide steps to reproduce the issue you're having.
+Current behavior:     A description of how Kap is currently behaving.
+Expected behavior:    How you expected Kap to behave.
+Workaround:           A workaround for the issue if you've found on. (this will help others experiencing the same issue!)
+-->
+
+**macOS version:**    ${process.platform} ${process.arch} ${os.release()}
+**Kap version:**      ${app.getVersion()}
+
+#### Steps to reproduce
+
+#### Current behavior
+
+#### Expected behavior
+
+#### Workaround
+
+<!-- If you have additional information, enter it below. -->
+`;
+
 const cogMenu = [
   {
     role: 'about'
@@ -35,7 +61,7 @@ const cogMenu = [
   checkForUpdatesItem,
   {
     label: 'Send Feedback…',
-    click: () => shell.openExternal('https://github.com/wulkano/kap/issues/new?template=beta.md')
+    click: () => shell.openExternal(`https://github.com/wulkano/kap/issues/new?body=${encodeURIComponent(issueBody)}`)
   },
   {
     type: 'separator'
