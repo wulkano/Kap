3e3608b6c73426d229d3efd7481cb4ed5f0e40e2
Clayton Ferris <cferris@gmail.com>
2018-10-06 22:47:29 -0700

    Position menu above IconMenu near screen bottom

--- renderer/components/icon-menu.js
+++ renderer/components/icon-menu.js
@@ -1,19 +1,26 @@
+import electron from 'electron';
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const MENU_UP_THRESHOLD = 60;
+
 class IconMenu extends React.Component {
+  remote = electron.remote || false
+
   container = React.createRef();
 
   openMenu = event => {
     const boundingRect = this.container.current.getBoundingClientRect();
-    const {bottom, left} = boundingRect;
+    const {screen} = this.remote;
+    const {bottom, left, top} = boundingRect;
+    const {height: screenHeight} = screen.getDisplayNearestPoint(screen.getCursorScreenPoint()).workArea;
     const {onOpen} = this.props;
     event.stopPropagation();
 
     if (onOpen) {
       onOpen({
         x: Math.round(left),
-        y: Math.round(bottom)
+        y: Math.round(screenHeight - bottom < MENU_UP_THRESHOLD ? top : bottom)
       });
     }
   }
