7643b07c7a5338074f8fa35c6b18a83d56256d7a
alonalon <aronhafner@gmail.com>
2018-11-01 22:59:23 +0100

    Fixes

--- main/editor.js
+++ main/editor.js
@@ -10,12 +10,36 @@ const loadRoute = require('./utils/routes');
 const editors = new Map();
 let exportOptions;
 let editorWindow;
-let forceClose = false;
 const OPTIONS_BAR_HEIGHT = 48;
 const VIDEO_ASPECT = 9 / 16;
 const MIN_VIDEO_WIDTH = 768;
 const MIN_VIDEO_HEIGHT = MIN_VIDEO_WIDTH * VIDEO_ASPECT;
 const MIN_WINDOW_HEIGHT = MIN_VIDEO_HEIGHT + OPTIONS_BAR_HEIGHT;
+
+const discardVideo = event => {
+  if (!editorWindow) {
+    return;
+  }
+
+  event.preventDefault();
+
+  const buttonIndex = dialog.showMessageBox(editorWindow, {
+    type: 'question',
+    buttons: [
+      'Discard',
+      'Cancel'
+    ],
+    defaultId: 0,
+    cancelId: 1,
+    message: 'Are you sure that you want to discard this recording?',
+    detail: 'You will no longer be able to edit and export the original recording.'
+  });
+
+  if (buttonIndex === 0) {
+    editorWindow.removeListener('close', discardVideo);
+    editorWindow.close();
+  }
+};
 
 const openEditorWindow = async (filePath, recordFps) => {
   if (editors.has(filePath)) {
@@ -42,29 +66,8 @@ const openEditorWindow = async (filePath, recordFps) => {
 
   loadRoute(editorWindow, 'editor');
 
-  editorWindow.on('close', event => {
-    if (!editorWindow || forceClose) {
-      return;
-    }
-
-    event.preventDefault();
-
-    dialog.showMessageBox(editorWindow, {
-      type: 'question',
-      buttons: ['Discard', 'Cancel'],
-      defaultId: 1,
-      message: 'Are you sure that you want to discard this recording?',
-      detail: 'You will no longer be able to edit and export the original recording.'
-    }, buttonIndex => {
-      if (buttonIndex === 0) {
-        forceClose = true;
-        editorWindow.close();
-      }
-    });
-  });
-
+  editorWindow.on('close', discardVideo);
   editorWindow.on('closed', () => {
-    forceClose = false;
     editorWindow = null;
     editors.delete(filePath);
     if (editors.size === 0) {
