71c0ae07e4b13415cd3a96416a0e469e8ce007f2
George Karagkiaouris <karaggeorge0@gmail.com>
2019-09-11 18:07:42 -0400

    Abstract plugin config to its own class

---
+++ main/utils/plugin-config.js
@@ -0,0 +1,57 @@
+const Store = require('electron-store');
+const Ajv = require('ajv');
+
+class PluginConfig extends Store {
+  constructor(pluginName, plugin) {
+    const defaults = {};
+
+    const validators = plugin.shareServices.filter(({config}) => Boolean(config)).map(service => {
+      const schemaProps = JSON.parse(JSON.stringify(service.config));
+      const requiredKeys = [];
+      for (const key of Object.keys(schemaProps)) {
+        if (!schemaProps[key].title) {
+          throw new Error('Config schema items should have a `title`');
+        }
+
+        if (schemaProps[key].required === true) {
+          delete schemaProps[key].required;
+          requiredKeys.push(key);
+        }
+      }
+
+      const schema = {
+        type: 'object',
+        properties: schemaProps,
+        required: requiredKeys
+      };
+
+      const ajv = new Ajv({
+        format: 'full',
+        useDefaults: true,
+        errorDataPath: 'property',
+        allErrors: true
+      });
+
+      const validator = ajv.compile(schema);
+      validator(defaults);
+      validator.title = service.title;
+      validator.config = service.config;
+
+      return validator;
+    });
+
+    super({
+      name: pluginName,
+      cwd: 'plugins',
+      defaults
+    });
+
+    this.validators = validators;
+  }
+
+  isConfigValid() {
+    return this.validators.reduce((isValid, validator) => isValid && validator(this.store), true);
+  }
+}
+
+module.exports = PluginConfig;
