fa7f711bc6ec9317f99a1f587d1b57e89b0eec3c
dhuang612 <dhuang@madisoncollege.edu>
2019-09-12 13:31:46 -0400

    setup ava with electron completed basic test
    added in npm packages
    
    resolving error
    
    made requested changes
    
    added chrome driver path
    
    added additional params
    
    removing trailing space
    
    removed trailing comma
    
    resolved electron version conflict
    
    resolved dependencies and switched from import to require

---
+++ test/test.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const test = require('ava');
+const {Application} = require('spectron');
+
+const electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
+const appPath = path.join(__dirname, '..');
+test.beforeEach(async t => {
+  t.context.app = new Application({
+    path: electronPath,
+    args: [appPath],
+    env: {ELECTRON_ENABLE_LOGGING: true, ELECTRON_ENABLE_STACK_DUMPING: true, NODE_ENV: 'development'},
+    startTimeout: 2000,
+    waitTimeout: 5000,
+    chromeDriverLogPath: '../chromedriverlog.txt'
+  });
+  await t.context.app.start();
+});
+
+test.afterEach.always(async t => {
+  await t.context.app.stop();
+});
+
+test('launch app and get information', async t => {
+  const {app} = t.context;
+  const {client} = app;
+  // Needs 2 windows because of dev tools
+  t.is(await client.getWindowCount(), 2);
+});
