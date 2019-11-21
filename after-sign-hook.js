7e57e523f543554817da526a5ff764463f21ac7e
Sindre Sorhus <sindresorhus@gmail.com>
2019-11-21 23:37:04 +0700

    Update after-sign-hook.js

--- after-sign-hook.js
+++ after-sign-hook.js
@@ -1,11 +1,11 @@
 // See: https://medium.com/@TwitterArchiveEraser/notarize-electron-apps-7a5f988406db
 
+'use strict';
 const fs = require('fs');
 const path = require('path');
 const electronNotarize = require('electron-notarize');
 
 module.exports = async function (params) {
-  // Only notarize the app on Mac OS only.
   if (process.platform !== 'darwin') {
     return;
   }
@@ -17,7 +17,7 @@ module.exports = async function (params) {
 
   const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
   if (!fs.existsSync(appPath)) {
-    throw new Error(`Cannot find application at: ${appPath}`);
+    throw new Error(`Cannot find app at: ${appPath}`);
   }
 
   console.log(`Notarizing ${appId} found at ${appPath}`);
