fc0ba2ebc6ae0171d2dc8a64e802dba0b0686eeb
George Karagkiaouris <karaggeorge0@gmail.com>
2019-11-21 11:52:14 -0500

    Clean up notarize script

--- notarize.js
+++ notarize.js
@@ -5,23 +5,20 @@ const fs = require('fs');
 const path = require('path');
 const electronNotarize = require('electron-notarize');
 
-module.exports = async function (params) {
+module.exports = async params => {
   if (process.platform !== 'darwin') {
     return;
   }
 
   // Only notarize the app on the master branch
-  if (process.env.CIRCLE_BRANCH !== 'master') {
-    return;
-  }
+  // if (process.env.CIRCLE_BRANCH !== 'master') {
+  //   return;
+  // }
 
-  // Same appId in electron-builder.
-  const appId = 'com.wulkano.kap';
+  const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+  const {appId} = packageJson.build;
 
   const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
-  if (!fs.existsSync(appPath)) {
-    throw new Error(`Cannot find app at: ${appPath}`);
-  }
 
   console.log(`Notarizing ${appId} found at ${appPath}`);
 
@@ -34,6 +31,7 @@ module.exports = async function (params) {
     });
   } catch (error) {
     console.error(error);
+    return;
   }
 
   console.log(`Done notarizing ${appId}`);
