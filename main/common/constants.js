f6cb06b17c74269c4d31d304c2f7a5e2dd3bfb75
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-06-19 14:57:55 -0400

    Capitalize default

--- main/common/constants.js
+++ main/common/constants.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const supportedVideoExtensions = ['mp4', 'mov', 'm4v'];
-const defaultInputDevice = {id: 'SYSTEM_DEFAULT', name: 'System default'};
+const defaultInputDevice = {id: 'SYSTEM_DEFAULT', name: 'System Default'};
 
 module.exports = {
   supportedVideoExtensions,
