05d8be8e4974fde5dc1ffefb65f0096ca5066b0b
Sindre Sorhus <sindresorhus@gmail.com>
2021-09-05 13:56:58 +0200

    Remove unused code and dependency

--- main/utils/icon.ts
+++
@@ -1,7 +0,0 @@
-
-const fileIcon = require('file-icon');
-
-export const getAppIcon = async (): Promise<string> => {
-  const buffer = await fileIcon.buffer(process.pid);
-  return buffer.toString('base64');
-};
