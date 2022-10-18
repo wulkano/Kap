6ead24246ce360ab0c7391a3c7b336bf97fb85dd
Thomas Zhou <thethomaszhou@gmail.com>
2022-10-17 22:53:39 -0700

    #129 Add TODO comment

--- main/utils/track-duration.ts
+++ main/utils/track-duration.ts
@@ -1,3 +1,4 @@
+// TODO: Add interface to aperture-node for getting recording duration instead of using this https://github.com/wulkano/aperture-node/issues/29
 let overallDuration = 0;
 let currentDurationStart = 0;
 
