6964d12541828ea7279214b9400a8a96a17e569e
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2024-10-15 22:05:28 -0400

    Refactor editor events

--- main/utils/window-controls.ts
+++ main/utils/window-controls.ts
@@ -6,7 +6,8 @@ export function initializeWindowControls() {
     return {
       closable: window.isClosable(),
       minimizable: window.isMinimizable(),
-      maximizable: window.isMaximizable()
+      maximizable: window.isMaximizable(),
+      isFocused: window.isFocused()
     };
   });
 
@@ -24,5 +25,26 @@ export function initializeWindowControls() {
     for (const window of BrowserWindow.getAllWindows()) {
       ipcMain.callRenderer(window, 'window-tint-changed', systemPreferences.getUserDefault('AppleAquaColorVariant', 'string'));
     }
+  });
+
+  ipcMain.answerRenderer('focus-events', async (_, window) => {
+    const onFocus = () => {
+      ipcMain.callRenderer(window, 'window-focus');
+    };
+
+    const onBlur = () => {
+      ipcMain.callRenderer(window, 'window-blur');
+    };
+
+    window.on('focus', onFocus);
+    window.on('blur', onBlur);
+
+    const cleanup = ipcMain.answerRenderer('stop-focus-events', async (_, cleanupWindow) => {
+      if (window.id === cleanupWindow.id) {
+        window.removeListener('focus', onFocus);
+        window.removeListener('blur', onBlur);
+        cleanup();
+      }
+    });
   });
 }
